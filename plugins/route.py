1a1344e81e599db51246d84f56d490d89577099d
ANONYMOUSE70 <103603128+ANONYMOUSE70@users.noreply.github.com>
2023-02-20 22:40:10 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -11,4 +11,4 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("F9Videos")
+    return web.json_response("F9Devs")
